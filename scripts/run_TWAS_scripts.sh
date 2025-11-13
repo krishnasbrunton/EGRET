1fcead940df663fd11cd3a968b3652b852c22ddf
Krishna Brunton <krishnabrunton@gmail.com>
2025-11-13 11:31:24 -0800

    added

---
+++ scripts/run_TWAS_scripts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+tissue=$1
+trait=$2
+gwas_sumstat_dir=$3
+$output_dir=$3
+
+Rscript 16.5_create_EGRET_pos.R \
+    -tissue $tissue
+
+ld=${home}/fusion_twas-master/LDREF/1000G.EUR.merged
+
+# run TWAS
+Rscript ${home}/fusion_twas-master/FUSION.assoc_test_trans.R \
+    --chr $chr \
+    --ref_ld_chr $ld \
+    --sumstats $gwas \
+    --weights $wgt \
+    --weights_dir $wgtdir \
+    --out ${out}/${output}.dat
+
+
+    
